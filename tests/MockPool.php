1005bc7da7cd1d67953dbb7e4318e7f0133efdee
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-01-10 07:27:53 +0100

    [2.x] Ensure void return type translates to null

--- tests/MockPool.php
+++ tests/MockPool.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace ReactParallel\Tests\Contracts;
 
 use Closure;
+use parallel\Future;
 use ReactParallel\Contracts\PoolInterface;
+
+use function parallel\run;
+use function sleep;
 
 final class MockPool implements PoolInterface
 {
@@ -22,7 +26,17 @@ final class MockPool implements PoolInterface
      */
     public function run(Closure $callable, array $args = []): mixed
     {
-        return $callable(...$args);
+        $future = run($callable, $args);
+
+        if ($future instanceof Future) {
+            while (! $future->done() && ! $future->cancelled()) {
+                sleep(1);
+            }
+
+            return $future->value();
+        }
+
+        return null;
     }
 
     public function close(): bool
